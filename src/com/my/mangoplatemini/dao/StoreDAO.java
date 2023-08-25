ac12f22673ae736ccc31e86e33d693660b4a4024
hongsikcho <jjdd9758@naver.com>
2023-08-25 09:12:21 +0900

    feat : StoreDAO 업데이트, 삭제 메서드 선언

--- src/com/my/mangoplatemini/dao/StoreDAO.java
+++ src/com/my/mangoplatemini/dao/StoreDAO.java
@@ -1,5 +1,17 @@
 package com.my.mangoplatemini.dao;
 
-public class StoreDAO {
+public class StoreDAO implements StoreInterface{
+
+	@Override
+	public void updateStore() {
+		
+		
+	}
+
+	@Override
+	public void deleteStore() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
