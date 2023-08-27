ab506b37b3187b68f48b01f339c101123bcb6d08
hongsikcho <jjdd9758@naver.com>
2023-08-27 19:03:02 +0900

    refactor : 상점수정 & 상점삭제 코드 수정

--- src/com/my/mangoplatemini/dao/StoreInterface.java
+++ src/com/my/mangoplatemini/dao/StoreInterface.java
@@ -6,7 +6,9 @@ import java.util.List;
 import com.my.mangoplatemini.dto.StoreDTO;
 
 public interface StoreInterface {
-	
+	/*
+	 * 스토어의 상세정보를 볼 수 있다.
+	 */
 	public void showStoreDetail(String business_no);
 
 	/*
@@ -21,6 +23,10 @@ public interface StoreInterface {
 	 */
 	public void deleteStore(String name);
 
+
+	/*
+	 * 스토어 하나의 정보를 가져올 수 있다.
+	 */
 	public StoreDTO showStoreOne(String business_no);
 
 }
