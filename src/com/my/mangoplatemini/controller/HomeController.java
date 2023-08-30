67158cb75382fa80f9bc06c70f3e641168306ec7
khakyy <116627270+khakyy@users.noreply.github.com>
2023-08-31 00:52:08 +0900

    Refactoring
    Refactoring

--- src/com/my/mangoplatemini/controller/HomeController.java
+++ src/com/my/mangoplatemini/controller/HomeController.java
@@ -3,27 +3,51 @@ package com.my.mangoplatemini.controller;
 import java.util.Scanner;
 
 public class HomeController {
-	MemberController memberController = new MemberController();
-	
-	public void init() {
-		Scanner sc = new Scanner(System.in);
-		int input = 0;
+    MemberController memberController = new MemberController();
 
-		System.out.println("------- 안녕하세요 망고플레이트입니다. -------");
-		System.out.println("1. 로그인\n");
-		System.out.println("2. 회원가입");
+    public void init() {
+        String asciiArt =
+                        "    //    //                                        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠶⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+                        "   //___ //      ___       //     //      ___       ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⣀⣤⣶⣶⣿⣷⣶⣶⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+                        "  / ___  /     //___) )   //     //     //   ) )   ⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+                        " //    //     //         //     //     //   / /⠀⠀⠀⠀⠀⠀⠀⠀⠀    ⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+                        "//    //     ((____     //     //     ((___/ /  ⠀⠀⠀⠀⠀⠀ ⠀⠀⠀  ⣿⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿ ⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀\n" +
+                        "                                                  ⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀\n" +
+                        " ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣤⡀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀\n" +
+                        "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣄⠈⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀\n" +
+                        "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⣿⣿⣿⣿           ⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+                        "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                                   ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+                        "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                                   ⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠍⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+                        "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                                   ⠈⠛⠿⢿⣿⣿⡿⠿⡟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+                        "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                                    ⠐⠿⠗⠿⠿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n";
 
-		try {
-			input = Integer.parseInt(sc.nextLine());
-		} catch (NumberFormatException e) {
-			System.out.println("※올바른 숫자를 입력해주세요.※");
-		}
+        System.out.println(asciiArt);
 
-		if (input == 1) {
-			memberController.login();
-		} else if (input == 2) {
-			memberController.createMember();
-		}
-	}
+        Scanner sc = new Scanner(System.in);
+        int input;
+
+        System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ안녕하세요! 망고플레이트입니다!ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\n");
+
+        while (true) {
+            System.out.println("1. 로그인\n");
+            System.out.println("2. 회원 가입");
+
+            try {
+                input = Integer.parseInt(sc.nextLine());
+
+                if (input == 1) {
+                    memberController.login();
+                    break;
+                } else if (input == 2) {
+                    memberController.createMember();
+                    break;
+                } else {
+                    System.out.println("※ 올바른 숫자를 입력해주세요.※\n");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("※ 숫자를 입력해주세요.※\n");
+            }
+        }
+    }
 
 }
